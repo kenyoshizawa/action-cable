b1574a17b6aeda6111f080ffd034cfe1f1482369
KEN YOSHIZAWA <kenyoshizawa@mbp.local>
2020-01-18 14:11:50 +0900

    jquery install

---
+++ app/assets/javascripts/channels/room.js
@@ -0,0 +1,18 @@
+App.room = App.cable.subscriptions.create("RoomChannel", {
+  connected: function() {},
+  disconnected: function() {},
+  received: function(data) {
+    return alert(data['message']);
+  },
+  speak: function(message) {
+    return @perform('speak', {
+      message: message
+    });
+  }
+  }, $(document).on('keypress', '[data-behavior~=room_speaker]', function(event) {
+    if (event.keyCode === 13) {
+      App.room.speak(event.target.value);
+      event.target.value = '';
+      return event.preventDefault();
+    }
+}));
