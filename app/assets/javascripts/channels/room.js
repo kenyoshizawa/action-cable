edc3effc4685bc777ee5e989808a0d79baff19e3
KEN YOSHIZAWA <kenyoshizawa@mbp.local>
2020-01-22 21:28:30 +0900

    Uncaught ReferenceError: $ is not defined

--- app/assets/javascripts/channels/room.js
+++ app/assets/javascripts/channels/room.js
@@ -1,18 +1,54 @@
-App.room = App.cable.subscriptions.create("RoomChannel", {
-  connected: function() {},
-  disconnected: function() {},
-  received: function(data) {
-    return alert(data['message']);
-  },
-  speak: function(message) {
-    return @perform('speak', {
-      message: message
-    });
-  }
-  }, $(document).on('keypress', '[data-behavior~=room_speaker]', function(event) {
-    if (event.keyCode === 13) {
-      App.room.speak(event.target.value);
-      event.target.value = '';
-      return event.preventDefault();
-    }
-}));
+// App.room = App.cable.subscriptions.create("RoomChannel", {
+//   connected: function() {},
+//   disconnected: function() {},
+//   received: function(data) {
+//   alert(data['message']);
+//   return $('#messages').append(data['message']);
+//   },
+//   speak: function(message) {
+//     return this.perform('speak', {
+//       message: message
+//     });
+//   }
+// }
+//
+// // document.on('keypress', '[data-behavior~=room_speaker]', function(event) {
+// //   if (event.keyCode === 13) {
+// //     App.room.speak(event.target.value);
+// //     event.target.value = '';
+// //     return event.preventDefault();
+// //   }
+// // });
+//
+//
+// // App.room = App.cable.subscriptions.create("RoomChannel", {
+// //   connected: function() {
+// //   },
+// //
+// //   disconnected: function() {
+// //   },
+// //
+// //   received: function(post) {
+// //     const posts = document.getElementById('posts')
+// //     posts.insertAdjacentHTML('afterbegin', post);
+// //     // posts.innerHTML += post
+// //
+// //   },
+// //
+// //   speak: function(content, music_id) {
+// //     return this.perform('speak', {content: content, music_id: music_id});
+// //   }
+// // });
+// //
+// // document.addEventListener('turbolinks:load', function(){
+// //   const input = document.getElementById('chat-input');
+// //   const button = document.getElementById('chat-button');
+// //   button.addEventListener('click', function(e){
+// //     e.preventDefault();
+// //     const content = input.value;
+// //     const music = document.getElementById('music-id')
+// //     const music_id = music.textContent;
+// //     App.room.speak(content, music_id);
+// //     input.value = '';
+// //   })
+// // });
